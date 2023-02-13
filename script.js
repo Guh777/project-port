e217e244295e850558b2411aaf184719774383ed
Gustavo Alexandre <luffycapudo@gmail.com>
2023-02-13 15:17:20 -0800

    primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function padrao() {
+  const botao = document.getElementById('botao')
+  botao.style.borderRadius = '20px'
+
+
+
+
+
+}
+
+
+
+
+function voltaaonormal() {
+  const botao = document.getElementById('botao')
+  botao.style.borderRadius = '10px'
+}
