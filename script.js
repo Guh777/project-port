cb7d8f756437c45c725ae87fd15d7497e2a9db95
Gustavo Alexandre <luffycapudo@gmail.com>
2023-02-24 16:42:19 -0800

    atualizooou

--- script.js
+++ script.js
@@ -2,23 +2,7 @@
 function padrao() {
   const botao = document.getElementById('botao')
   botao.style.borderRadius = '20px'
-
-
-
-
-
 }
-
-function animation(event) {
-  event.preventDefault();
-  const target = event.target;
-  const secao = document.querySelector(target.getAttribute('href'));
-
-  secao.scrollIntoView({
-    behavior: 'smooth'
-  });
-};
-
 
 function voltaaonormal() {
   const botao = document.getElementById('botao')
